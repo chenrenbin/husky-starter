bbb373f5528c8e39a939db8eb40ee22c1ecee642
chenrenbin <chenrenbin@leelen.cn>
2022-03-27 13:47:23 +0800

    feat: husky-eslint-prettier

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    eqeqeq: 'error',
+    'no-var': 'error'
+  }
+}
